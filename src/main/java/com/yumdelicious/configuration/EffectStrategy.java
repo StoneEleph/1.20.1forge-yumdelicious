dd065e80dc43947aa7ff2753ae94e348e9ae6f83
StoneEleph <stoneelephemail@163.com>
2025-09-13 22:40:07 +0800

    Delete src directory

--- src/main/java/com/yumdelicious/configuration/EffectStrategy.java
+++
@@ -1,8 +0,0 @@
-package yum.configuration;
-
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.Item;
-
-public interface EffectStrategy {
-    void applyEffect(LivingEntity livingEntity, Item consumedItem);
-}
